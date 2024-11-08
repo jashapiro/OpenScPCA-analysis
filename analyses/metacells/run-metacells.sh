d229dc10798e70e16850a2033fccd7b06af237dd
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-11-08 14:53:53 -0500

    Add quarto notebook and requirements

--- analyses/metacells/run-metacells.sh
+++ analyses/metacells/run-metacells.sh
@@ -19,14 +19,17 @@ mkdir -p ${results_dir}
 # run the SEACells script
 echo "Calculating SEACells..."
 python scripts/run-seacells.py \
-    ${test_file} \
-    --adata_out ${results_dir}/${library}_seacells.h5ad \
-    --model_out ${results_dir}/${library}_seacells_model.pkl \
-    --logfile ${results_dir}/${library}_seacells.log \
-    --seed 2024
+  ${test_file} \
+  --adata_out ${results_dir}/${library}_seacells.h5ad \
+  --model_out ${results_dir}/${library}_seacells_model.pkl \
+  --logfile ${results_dir}/${library}_seacells.log \
+  --seed 2024
+
+# set up the rendering environment for the metacells notebook
+ipython kernel install --user --name=openscpca-metacells
 
 echo "Rendering test notebook"
-Rscript -e "rmarkdown::render(
-    'notebooks/testing-seacells.Rmd',
-    params=list(project_id='${project}', sample_id='${sample}', library_id='${library}')
-  )"
+quarto render notebooks/testing-seacells.qmd \
+  -P project_id:${project} \
+  -P sample_id:${sample} \
+  -P library_id:${library}
